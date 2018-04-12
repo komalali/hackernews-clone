a045085e066bb2b61847d36aef8106a1b483a542
Komal Ali <komalali@uw.edu>
2018-04-11 22:03:22 -0700

    create prisma database api layer

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 const { GraphQLServer } = require('graphql-yoga');
-
-const typeDefs= `
-type Query {
-  info: String!
-}
-`;
+const { Prisma } = require('prisma-binding');
 
 const resolvers = {
   Query: {
-    info: () => `This is the API of a Hackernews Clone`
+    info: () => `This is the API of a Hackernews Clone`,
+    feed: (root, args, context, info) => {
+      return context.db.query.links({}, info)
+    },
+  },
+  Mutation: {
+    post: (root, args, context, info) => {
+      return context.db.mutation.createLink({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      }, info)
+    },
   }
 };
 
 const server = new GraphQLServer({
-  typeDefs,
+  typeDefs: './src/schema.graphql',
   resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      typeDefs: 'src/generated/prisma.graphql',
+      endpoint: 'https://us1.prisma.sh/public-treestinger-98/hackernews-node/dev',
+      secret: 'mysecret123',
+      debug: true,
+    })
+  })
 });
 
 server.start(() => console.log(`Server is running on http://localhost:4000`));
