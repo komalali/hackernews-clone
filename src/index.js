0871170032581007c41ffbacdeca1a60f8ee7717
Komal Ali <komalali@uw.edu>
2018-04-14 14:19:19 -0700

    adding pagination and filters

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 const { GraphQLServer } = require('graphql-yoga');
 const { Prisma } = require('prisma-binding');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const AuthPayload = require('./resolvers/AuthPayload');
+const Subscription = require('./resolvers/Subscription');
 
 const resolvers = {
-  Query: {
-    info: () => `This is the API of a Hackernews Clone`,
-    feed: (root, args, context, info) => {
-      return context.db.query.links({}, info)
-    },
-  },
-  Mutation: {
-    post: (root, args, context, info) => {
-      return context.db.mutation.createLink({
-        data: {
-          url: args.url,
-          description: args.description,
-        },
-      }, info)
-    },
-  }
+  Query,
+  Mutation,
+  AuthPayload,
+  Subscription,
 };
 
 const server = new GraphQLServer({
